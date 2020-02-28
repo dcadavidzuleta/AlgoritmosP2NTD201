333c2af464b29a12bed4fd3778b1db1a11e23753
204 <204@academicos.cesde.edu.co>
2020-02-27 19:52:53 -0500

    cambio ejercicio1

--- src/algoritmosp2ntp201/AlgoritmosP2NTP201.java
+++ src/algoritmosp2ntp201/AlgoritmosP2NTP201.java
@@ -5,17 +5,44 @@
  */
 package algoritmosp2ntp201;
 
-/**
- *
- * @author 204
- */
+import static java.time.Clock.system;
+import java.util.Scanner;
+
 public class AlgoritmosP2NTP201 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    // Entrada    
+        int rc = 0;
+        int ri = 0;
+        int rb = 0;
+        int prc = 4;
+        int pri = -1;
+        int prb = 0;
+        
+        // libreria para pedir por datos al usuario y convertirlos en una variable
+        Scanner sc = new Scanner(System.in);
+        
+    // Proceso
+        System.out.println("Ingresa cantidad de respuestas correctas");
+        rc = sc.nextInt();
+        
+        System.out.println("Ingresa cantidad de respuestas incorrectas");
+        ri = sc.nextInt();
+        
+        System.out.println("Ingresa cantidad de respuestas en blanco");
+        rb = sc.nextInt();
+        
+    // Salida
+        System.out.println("///////***************/////////");
+        System.out.println("Tu puntuación");
+        System.out.println("///////***************/////////");
+        System.out.println((rc * prc) 
+                + (ri * pri)
+                + (rb * prb));
+        
+      
+        
     }
     
 }
